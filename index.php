d95fc9d521fd5b08b218605fdf1c4aae1c838771
ISABELA VICTORIA DE NOVAIS ROMANATO <21193062@CFP127ALUNO.LOCAL>
2021-08-05 11:28:51 -0300

    inicio do calculo

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+print_r($_POST);
+
+$nota1;
+$nota2;
+$nota3;
+
+
+$mediaNotas = ($nota1 + $nota2 + $nota3) / 3;
+
+echo "A sua média é: $mediaNotas"; 
+
+
+if($mediaNotas == >6 == true ){
+    echo "Você está APROVADO!!!";
+
+} if else ($mediaNotas == >6 == true ) {
+    echo "Você não é javeiro";
+
+}
+
