7e54c3803ffd90ed0bf360c820cd691d32784754
romanato15 <isa.romanato@gmail.com>
2021-08-05 23:17:19 -0300

    continuidade do exercício

--- index.php
+++ index.php
@@ -2,21 +2,45 @@
 
 print_r($_POST);
 
-$nota1;
-$nota2;
-$nota3;
+// $nome;
+// $nota1;
+// $nota2;
+// $nota3;
+
+$nome= $_POST["nome"];
+$nota1= $_POST["nota1"];
+$nota2= $_POST["nota2"];
+$nota3= $_POST["nota3"];
 
 
-$mediaNotas = ($nota1 + $nota2 + $nota3) / 3;
-
-echo "A sua média é: $mediaNotas"; 
+$mediaNotas= ($nota1 + $nota2 + $nota3) / 3;
 
 
-if($mediaNotas == >6 == true ){
-    echo "Você está APROVADO!!!";
 
-} if else ($mediaNotas == >6 == true ) {
-    echo "Você não é javeiro";
+echo "A média de $nome é: $mediaNotas"; 
 
-}
 
+
+if($mediaNotas>6){
+    echo "O(a) aluno(a) $nome foi Aprovado(a)!!!";
+    }else
+    if(($mediaNotas<4) && ($mediaNotas>=6)){
+    echo "O(a) aluno(a) $nome está de Recuperação!!!";
+    }else{
+     echo "O(a) aluno(a) $nome foi Reprovado!!!";
+     }
+
+    //  switch  {
+    //     case $mediaNotas == "O(a) aluno(a) $nome foi Aprovado(a)!!!";
+
+    //           //código referente ao valor1
+    //           break;
+    //     case valor2:
+    //           //código referente ao valor2
+    //           break;
+    //     case valorN:
+    //           //código referente ao valorN
+    //           break;
+    //     default:
+    //           //código caso nenhuma condição seja atendida
+    //           break;
